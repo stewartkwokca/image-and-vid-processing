eac5118b0f75097cef2d68683a5be3c27f2fa9f7
Stewart Kwok <stewart.kwok1@gmail.com>
2023-09-10 09:13:47 -0700

    Fix cutByTimestamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# Video Processing Functions
-
 from pytube import YouTube
 from moviepy.editor import *
+from google_images_download import google_images_download
 
 def downloadYT(link):
     youtubeObject = YouTube(link)
@@ -24,7 +23,7 @@ def extractAudioClip(vidFile, t1, t2, outPath):
     audioClip = AudioFileClip(vidFile).subclip(t1, t2)
     audioClip.write_audiofile(outPath)
 
-def cutByTimestamp(audioFile, timeStampFile, outputFolderName):
+def cutByTimestamp(audioFile, timeStampFile):
     with open(timeStampFile) as file:
         timeInSeconds = []
         outPaths = []
@@ -37,10 +36,10 @@ def cutByTimestamp(audioFile, timeStampFile, outputFolderName):
     print(timeInSeconds)
     print(outPaths)
     for i in range(len(outPaths)):
-        clip = fullAudioClip.subclip(timeInSeconds[i]+1, timeInSeconds[i+1])
+        clip = fullAudioClip.subclip(timeInSeconds[i]+0.9, timeInSeconds[i+1])
         clip.write_audiofile(outPaths[i])
 
 #link = "https://www.youtube.com/watch?v=Z_BHv6RFVpY"
 #downloadYT(link)
-#clip("test.mp4", 913, 916, "outputs/clip_test.mp4")
-#cutByTimestamp("inputs/cold_edit_songs.mp4", 'timeStamps.txt', "edit-songs")
+#clip("downloadedYTvids/test.mp4", 913, 916, "clippdVids/clip_test.mp4")
+#cutByTimestamp("downloadedYTvids/cold_edit_songs.mp4", 'timeStamps.txt')
